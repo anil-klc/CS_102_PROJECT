5459f63b86222d9d83f54d8cde96eca82b131824
anil <anilkilic48@hotmail.com>
2023-02-08 11:15:39 +0300

    final product

--- console_app.java
+++ console_app.java
@@ -5,7 +5,50 @@ import java.util.*;
 public class console_app {
     public static void main(String[] args) {
 
+        boolean exit = false;
+
+        int user_chose;
+
+        int arr_size;
+        
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Provide the size of the array : ");
+
+        arr_size = in.nextInt();
+
+        int[] user_array = array_maker(arr_size);
+        
+
+        do {
+            
+            displayMenu();
+
+            user_chose = getUserAnswer();
+
+            if (user_chose == 1) {
+                System.out.println(findMin(user_array));
+            }
+            else if(user_chose == 2){
+                System.out.println(findMax(user_array));
+            }
+            else if(user_chose == 3){
+                findDifferencesFromAverage(user_array);   
+            }
+            else if(user_chose == 4){
+                System.out.println(sumOdds(user_array));
+            }
+            else if(user_chose == 5){
+                System.out.println(sumEvens(user_array));
+            }
+            else if(user_chose == 6){
+                exit = true;
+            }
+
+        } while( !exit );
+         
     }
+    
 
     public static int[] array_maker(int array_size) {
 
@@ -18,26 +61,28 @@ public class console_app {
         return rand_int_array;
     }
 
-    public void displayMenu(){
+    public static void displayMenu(){
+        System.out.print("------------------------------- \n");
+        System.out.println("------------MENU-------------");
         System.out.print("------------------------------- \n");
         System.out.print("1-Find the minimum value in the array\n");
         System.out.print("2-Find the maximum value in the array\n");
-        System.out.print("3-Find the differences from the average\n");
+        System.out.print("3-Find the average distance of the array\n");
         System.out.print("4-Find the sum of odd indexes in the array\n");
         System.out.print("5-Find the sum of the even indexes in the array\n");
         System.out.print("6-Exit");
-        System.out.print("-------------------------------");
+        System.out.print("-------------------------------\n");
     }
 
-    public int getUserAnswer(){
+    public static int getUserAnswer(){
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter your choice;");
+        System.out.print("Enter your choice : ");
         int returnValue = in.nextInt();
 
         return returnValue;
     }
 
-    public int sumEvens(int[] inputArray){
+    public static int sumEvens(int[] inputArray){
         int totalSum = 0;
         for(int i = 0; i < inputArray.length; i++){
             if(i%2 == 0){
@@ -47,7 +92,7 @@ public class console_app {
         return totalSum;
     }
 
-    public int sumOdds(int[] inputArray){
+    public static int sumOdds(int[] inputArray){
         int totalSum = 0;
         for(int i = 0; i < inputArray.length; i++){
             if(i%2 != 0){
@@ -57,7 +102,7 @@ public class console_app {
         return totalSum;
     }
 
-    public void findDifferencesFromAverage(int [] array)
+    public static void findDifferencesFromAverage(int [] array)
     {
         int array_size= array.length;
         int sum = 0;
@@ -78,11 +123,21 @@ public class console_app {
             diferences_from_average[i] =array[i] - average;
         }
 
-        System.out.println(diferences_from_average);
+        System.out.print("{");
+        
+        for (int i = 0 ; i < array_size ; i++)
+        {
+            System.out.print(diferences_from_average[i]);
+            System.out.print(", ");
+        }
+
+        System.out.println("}");
+
+
     }
 
 
-    public int findMax(int [] array){
+    public static int findMax(int [] array){
 
         int max = Integer.MIN_VALUE;
 
@@ -94,12 +149,12 @@ public class console_app {
         return max;
     }
 
-    public int findMin(int [] array){
+    public static int findMin(int [] array){
 
         int min = Integer.MAX_VALUE;
 
         for(int i =0; i<array.length;i++){
-            if(array[i]>=min){
+            if(array[i]<=min){
                 min= array[i];
             }
         }
